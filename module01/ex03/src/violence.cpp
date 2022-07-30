b1ad315f4e4aab387512ac53b919f98d0504f7e5
jcorneli <jaybart@protonmail.com>
2022-07-30 03:15:08 +0200

    bit more main

--- module01/ex03/src/violence.cpp
+++ module01/ex03/src/violence.cpp
@@ -9,6 +9,13 @@ int main ( ){
 
 	Weapon	club = Weapon("crude spiked club");
 	HumanA	test_human1 = HumanA("bob", club);
+	HumanA	test_human2;
+
+	test_human1.attack();
+	test_human2.attack();
+	test_human2 = test_human1;
+	test_human1.attack();
+	test_human2.attack();
 
 	return 0;
 }
