e47d50e109cbdd93854158cde4740e9ff6fd0257
jcorneli <jaybart@protonmail.com>
2022-07-30 18:43:56 +0200

    nullptr definition in proper place..

--- module01/ex03/src/HumanB.hpp
+++ module01/ex03/src/HumanB.hpp
@@ -1,8 +1,11 @@
 #ifndef HUMANB_H
 # define HUMANB_H
 
-# ifndef nullptr
+# include <iostream>
+# include <sstream>
+# include "Weapon.hpp"
 
+# ifndef nullptr
 const                         /* this is a const object...     */
 class nullptr_t
 {
@@ -13,18 +16,13 @@ public:
 
    template<class C, class T> /* or any type of null           */
       operator T C::*() const /* member pointer...             */
-      { return 0; }   
+      { return 0; }
 
 private:
    void operator&() const;    /* Can't take address of nullptr */
 } nullptr = {};               /* and whose name is nullptr     */
-
-
 # endif
 
-# include <iostream>
-# include <sstream>
-# include "Weapon.hpp"
 
 class HumanB {
 
