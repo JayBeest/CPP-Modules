0f5e05b08be8f24d64bfe759c9a4335995c967f6
jcorneli <jaybart@protonmail.com>
2022-08-11 21:44:43 +0200

    more tests added

--- module04/ex01/src/main.cpp
+++ module04/ex01/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "Cat.hpp"
 #include "Dog.hpp"
 #include "WrongCat.hpp"
@@ -52,6 +53,40 @@ void	wrong_test( void )
 	delete i;
 }
 
+void	print_brain(Cat & cat)
+{
+	for (int i = 0; i < MAX_IDEAS; i++)
+	{
+		if (cat.accessBrain().getIdea(i).empty())
+			return;
+		std::cout << cat << "'s brain(" << i << "): " << cat.accessBrain().getIdea(i) << std::endl;
+	}
+}
+
+void	test_brain()
+{
+	Cat	cat_a("cat_a");
+	Cat cat_b("cat_b");
+
+	print_brain(cat_a);
+	std::cout << std::endl;
+	for (int i = 0; i < MAX_IDEAS/2; i++)
+	{
+		std::stringstream	s_stream;
+
+		s_stream << i;
+		cat_a.accessBrain().addIdea("ideetje: " + s_stream.str());
+	}
+	print_brain(cat_a);
+	std::cout << std::endl;
+
+	print_brain(cat_b);
+	std::cout << std::endl;
+	cat_b = cat_a;
+	print_brain(cat_b);
+	std::cout << std::endl;
+}
+
 int main (int argc, char **argv)
 {
 	if (argc == 2 && std::string(argv[1]) == "-s")
@@ -88,8 +123,9 @@ int main (int argc, char **argv)
 	j.makeSound();
 	std::cout << k << ": ";
 	k.makeSound();
-
 	delete i;
+
+	test_brain();
 
 	return 0;
 }
