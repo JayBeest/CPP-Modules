59fe82ac593d769f71dd51644ded62bbdd3426cf
jcorneli <jaybart@protonmail.com>
2022-08-11 19:11:13 +0000

    fix leak

--- module04/ex01/src/Cat.cpp
+++ module04/ex01/src/Cat.cpp
@@ -30,8 +30,7 @@ Cat::Cat( const Cat& other) {
 
 	if (this != &other)
 	{
-		delete this->_brain;
-	  *this = other;
+		*this = other;
 	}
 	if (Animal::_loud)
   		std::cout << "[Cat] Copy constructor called" << std::endl;
@@ -48,7 +47,7 @@ Cat &	Cat::operator=( const Cat& rhs ) {
 
 	if (this != &rhs)
 	{
-//		delete this->_brain;
+		delete this->_brain;
 		this->type = rhs.getType();
 		this->_brain = new Brain(rhs.accessBrain());
 	}
