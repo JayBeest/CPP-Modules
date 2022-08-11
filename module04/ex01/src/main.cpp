929a98a6f8c7cc19f9f91f63ff0526593fe08ac9
jcorneli <jaybart@protonmail.com>
2022-08-11 21:48:53 +0200

    bit cleanup

--- module04/ex01/src/main.cpp
+++ module04/ex01/src/main.cpp
@@ -4,55 +4,6 @@
 #include "Dog.hpp"
 #include "WrongCat.hpp"
 
-void	test( void )
-{
-//	Animal::makeSilent();
-//	Cat::makeSilent();
-//	Dog::makeSilent();
-
-	Animal	animal("Fran");
-	Cat		cat("Stein");
-	Dog		dog("Ken");
-	Dog		clean_dog;
-	Animal	*ptr;
-	Cat		*cat_ptr;
-
-	std::cout << std::endl << animal << dog << cat << std::endl << std::endl;
-
-	ptr = &animal;
-	std::cout << *ptr << ": ";
-	ptr->makeSound();
-	ptr = &dog;
-	std::cout << *ptr << ": ";
-	ptr->makeSound();
-	ptr = &cat;
-	std::cout << *ptr << ": ";
-	ptr->makeSound();
-
-	cat_ptr = (Cat *)&clean_dog;
-	std::cout << *cat_ptr << ": ";
-	cat_ptr->makeSound();
-
-}
-
-void	wrong_test( void )
-{
-	const WrongAnimal* meta = new WrongAnimal();
-	const WrongAnimal* i = new WrongCat();
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound();
-	meta->makeSound();
-
-	const WrongCat *ptr = (WrongCat *)i;
-	ptr->makeSound();
-
-	ptr = (WrongCat *)meta;
-	ptr->makeSound();
-
-	delete meta;
-	delete i;
-}
-
 void	print_brain(Cat & cat)
 {
 	for (int i = 0; i < MAX_IDEAS; i++)
@@ -70,7 +21,7 @@ void	test_brain()
 
 	print_brain(cat_a);
 	std::cout << std::endl;
-	for (int i = 0; i < MAX_IDEAS/2; i++)
+	for (int i = 0; i < MAX_IDEAS*2; i++)
 	{
 		std::stringstream	s_stream;
 
@@ -94,24 +45,7 @@ int main (int argc, char **argv)
 		Animal::makeSilent();
 		WrongAnimal::makeSilent();
 	}
-//	const Animal* meta = new Animal();
-//	const Animal* j = new Dog();
-//	const Animal* i = new Cat();
-//	std::cout << j->getType() << " " << std::endl;
-//	std::cout << i->getType() << " " << std::endl;
-//	i->makeSound(); //will output the cat sound!
-//	j->makeSound();
-//	meta->makeSound();
-//	delete meta;
-//	delete i;
-//	delete j;
-//
-//	std::cout << std::endl << "Starting test:" << std::endl << std::endl;
-//	test();
-//	std::cout << std::endl << "Starting wrong_test:" << std::endl << std::endl;
-//	wrong_test();
 
-//	const Animal* j = new Dog();
 	const Animal*	i = new Cat();
 	Cat				j;
 	Animal			k;
