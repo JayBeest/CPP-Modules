eac467e90d592f432510b2d3a08da52bfe2b34dc
jcorneli <jaybart@protonmail.com>
2022-08-12 05:26:18 +0000

    more tests

--- module04/ex03/src/main.cpp
+++ module04/ex03/src/main.cpp
@@ -14,6 +14,19 @@ int main ( ){
 	a->equip(new Ice);
 	a->use(0, *a);
 	a->use(1, *a);
+	a->use(2, *a);
+	a->use(3, *a);
+	a->use(4, *a);
+	a->unequip(3);
+	a->unequip(1);
+	a->use(3, *a);
+	a->use(1, *a);
+	a->equip(new Ice);
+	a->equip(new Ice);
+	a->equip(new Ice);
+	a->equip(new Ice);
 
+
+	delete a;
 	return 0;
 }
