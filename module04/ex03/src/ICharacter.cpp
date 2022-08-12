2adf5e3d58e10b88e91d7469713b86d5a3ad513f
jcorneli <jaybart@protonmail.com>
2022-08-12 07:58:33 +0200

    cleanup

--- module04/ex03/src/ICharacter.cpp
+++ module04/ex03/src/ICharacter.cpp
@@ -1,58 +1,7 @@
 #include <iostream>
 #include "ICharacter.hpp"
 
-///			Public:
-
-///			Getters / Setters
-
-///			Constructor / Destructor
-
-//ICharacter::ICharacter( ) {
-//
-//	if (AMateria::_loud)
-//		std::cout << "[ICharacter] Default constructor called" << std::endl;
-//}
-
-//ICharacter::ICharacter( const ICharacter& other) {
-//
-//	if (this != &other)
-//	{
-//	  *this = other;
-//	}
-//	if (AMateria::_loud)
-//		std::cout << "[ICharacter] Copy constructor called" << std::endl;
-//}
-
 ICharacter::~ICharacter( ) {
 
-	// TODO
-//	if (AMateria::_loud)
 		std::cout << "[ICharacter] Destructor called" << std::endl;
 }
-
-//ICharacter &	ICharacter::operator=( const ICharacter& rhs ) {
-//
-//	if (this != &rhs)
-//	{
-//	}
-//	if (AMateria::_loud)
-//		std::cout << "[ICharacter] Copy assignment operator called" << std::endl;
-//	return *this;
-//}
-
-///			Functions / Methods
-
-//void	ICharacter::equip(AMateria *m) {
-//
-//}
-//
-//void	ICharacter::unequip(int idx) {
-//
-//}
-//
-//void	ICharacter::use(int idx, ICharacter &target) {
-//
-//}
-
-///			Private:
-
