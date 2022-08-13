7013d6c1cd61fda2e9c1a504eb21fdfc6b42fb39
jcorneli <jaybart@protonmail.com>
2022-08-13 12:35:51 +0200

    cleanup

--- module00/ex02/src/Account.cpp
+++ module00/ex02/src/Account.cpp
@@ -2,13 +2,6 @@
 #include <iomanip>
 #include "Account.hpp"
 
-int	Account::_nbAccounts = 0;
-int	Account::_totalAmount = 0;
-int	Account::_totalNbDeposits = 0;
-int	Account::_totalNbWithdrawals = 0;
-
-using namespace::std;
-
 // Public:
 
 int		Account::getNbAccounts( void ) {
@@ -34,11 +27,11 @@ int		Account::getNbWithdrawals( void ) {
 void	Account::displayAccountsInfos( void ) {
 
 	_displayTimestamp();
-	cout << "accounts:" << getNbAccounts() << ";"
+	std::cout << "accounts:" << getNbAccounts() << ";"
 		<< "total:" << getTotalAmount() << ";"
 		<< "deposits:" << getNbDeposits() << ";"
 		<< "withdrawals:" << getNbWithdrawals()
-		<< endl;
+		<< std::endl;
 }
 
 
@@ -47,57 +40,53 @@ Account::Account( int initial_deposit ) : _amount(initial_deposit) {
 	this->_accountIndex = this->_nbAccounts++;
 	this->_totalAmount += initial_deposit;
 	_displayTimestamp();
-	cout << "index:" << this->_accountIndex << ";"
+	std::cout << "index:" << this->_accountIndex << ";"
 		<< "amount:" << this->_amount << ";"
-		<< "created" << endl;
-
+		<< "created" << std::endl;
 }
 
 Account::~Account( void ) {
 
 	_displayTimestamp();
-	cout << "index:" << this->_accountIndex << ";"
+	std::cout << "index:" << this->_accountIndex << ";"
 		 << "amount:" << this->_amount << ";"
-		 << "closed" << endl;
-
+		 << "closed" << std::endl;
 }
 
 
 void	Account::makeDeposit( int deposit ) {
 
 	_displayTimestamp();
-	cout << "index:" << this->_accountIndex << ";"
+	std::cout << "index:" << this->_accountIndex << ";"
 		 << "p_amount:" << this->checkAmount() << ";";
 	this->_amount += deposit;
 	this->_nbDeposits++;
 	_totalAmount += deposit;
 	_totalNbDeposits++;
-	cout << "deposit:" << deposit << ";"
+	std::cout << "deposit:" << deposit << ";"
 		 << "amount:" << this->checkAmount() << ";"
 		 << "nb_deposits:" << this->_nbDeposits
-		 << endl;
-
+		 << std::endl;
 }
 
 bool	Account::makeWithdrawal( int withdrawal ) {
 
 	_displayTimestamp();
-	cout << "index:" << this->_accountIndex << ";"
+	std::cout << "index:" << this->_accountIndex << ";"
 		 << "p_amount:" << this->checkAmount() << ";";
 	if (withdrawal > this->checkAmount())
 	{
-		cout << "withdrawal:refused" << endl;
+		std::cout << "withdrawal:refused" << std::endl;
 		return (false);
 	}
 	this->_amount -= withdrawal;
 	this->_nbWithdrawals++;
 	_totalAmount -= withdrawal;
 	_totalNbWithdrawals++;
-	cout << "withdrawal:" << withdrawal << ";"
+	std::cout << "withdrawal:" << withdrawal << ";"
 		<< "amount:" << this->checkAmount() << ";"
 		<< "nb_withdrawals:" << this->_nbWithdrawals
-		<< endl;
-
+		<< std::endl;
 	return (true);
 }
 
@@ -109,12 +98,11 @@ int		Account::checkAmount( void ) const {
 void	Account::displayStatus( void ) const {
 
 	_displayTimestamp();
-	cout << "index:" << this->_accountIndex << ";"
+	std::cout << "index:" << this->_accountIndex << ";"
 		<< "amount:" << this->checkAmount() << ";"
 		<< "deposits:" << this->_nbDeposits << ";"
 		<< "withdrawals:" << this->_nbWithdrawals
-		<< endl;
-
+		<< std::endl;
 }
 
 // Private:
@@ -126,16 +114,18 @@ void	Account::_displayTimestamp( void ) {
 
 	time(&rawtime);
 	timeinfo = localtime(&rawtime);
-	cout << "["
+	std::cout << "["
 		<< 1900 + timeinfo->tm_year
-		<< setw(2) << setfill('0') << timeinfo->tm_mon + 1
-		<< setw(2) << setfill('0') << timeinfo->tm_mday
+		<< std::setw(2) << std::setfill('0') << timeinfo->tm_mon + 1
+		<< std::setw(2) << std::setfill('0') << timeinfo->tm_mday
 		<< "_"
-		<< setw(2) << setfill('0') << timeinfo->tm_hour
-		<< setw(2) << setfill('0') << timeinfo->tm_min
-		<< setw(2) << setfill('0') << timeinfo->tm_sec
+		<< std::setw(2) << std::setfill('0') << timeinfo->tm_hour
+		<< std::setw(2) << std::setfill('0') << timeinfo->tm_min
+		<< std::setw(2) << std::setfill('0') << timeinfo->tm_sec
 		<< "] ";
-
 }
 
-
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
