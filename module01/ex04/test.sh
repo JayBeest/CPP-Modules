c2e6081c574c1c16632de6e9339ca8c1b1927b5e
jcorneli <jaybart@protonmail.com>
2022-08-21 22:02:53 +0200

    test result msg and files

--- module01/ex04/test.sh
+++ module01/ex04/test.sh
@@ -4,7 +4,24 @@ if ! [[ $1 ]] || ! [[ $2 ]] || ! [[ $3 ]]; then
   echo "needs 3 args"
 else
   bin/loser_sed "$1" "$2" "$3"
-  sed s/"$2"/"$3"/g "$1" > "$1".sed
-  diff "$1".replace "$1".sed
+
+  if [ "$2" == '$' ]; then
+    arg1='\$'
+  else
+    arg1="$2"
+  fi
+  if [ "$3" == '$' ]; then
+    arg2='\$'
+  else
+    arg2="$3"
+  fi
+
+  sed s/"$arg1"/"$arg2"/gi "$1" > "$1".sed
+  echo "Written changes to: '$1.sed'"
+  if [[ $(diff "$1".replace "$1".sed) != "" ]]; then
+    echo "diff found.. check '$1.replace' and '$1.sed'"
+  else
+    echo "no diff between '$1.replace' and '$1.sed' !"
+  fi
 fi
 
