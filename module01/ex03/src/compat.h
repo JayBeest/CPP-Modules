e6a34a2abfe140c1c4089278f8b2548a2f9797a1
jcorneli <jaybart@protonmail.com>
2022-08-21 19:25:03 +0200

    nullptr compatibility to compat.h

---
+++ module01/ex03/src/compat.h
@@ -0,0 +1,24 @@
+#ifndef COMPAT_H
+# define COMPAT_H
+
+# ifndef nullptr
+
+	const                         /* this is a const object...     */
+	class nullptr_t
+	{
+	public:
+	   template<class T>          /* convertible to any type       */
+	   operator T*() const        /* of null non-member            */
+	      { return 0; }           /* pointer...                    */
+
+	   template<class C, class T> /* or any type of null           */
+	      operator T C::*() const /* member pointer...             */
+	      { return 0; }
+
+	private:
+	   void operator&() const;    /* Can't take address of nullptr */
+	} nullptr = {};               /* and whose name is nullptr     */
+
+# endif
+
+#endif
