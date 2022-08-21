1e4c040e79b7fa095917a2fd92cbfa9083bdfb24
jcorneli <jaybart@protonmail.com>
2022-08-21 19:11:09 +0200

    cosmetics

--- module01/ex02/src/hi_brain.cpp
+++ module01/ex02/src/hi_brain.cpp
@@ -3,8 +3,8 @@
 int main ( ){
 
 	std::string	str = "HI THIS IS BRAIN";
-	std::string	*stringPTR= &str;
-	std::string	&stringREF= str;
+	std::string	* stringPTR= &str;
+	std::string	& stringREF= str;
 
 	std::cout << "address of str:        " << &str << std::endl;
 	std::cout << "value of stringPTR:    " << stringPTR << std::endl;
