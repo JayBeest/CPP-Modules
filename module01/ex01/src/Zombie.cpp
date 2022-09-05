564031a5555ea5a2a1af367a9bbfa3e52d7f5c52
Jay Bart Cornelisse <jcorneli@student.codam.nl>
2022-09-05 15:29:52 +0200

    simplified + tests

--- module01/ex01/src/Zombie.cpp
+++ module01/ex01/src/Zombie.cpp
@@ -16,35 +16,12 @@ Zombie::Zombie( const std::string & name )
 	Zombie::_id_counter++;
 }
 
-Zombie::Zombie( )
-	: _id(Zombie::_id_counter), _string("<no_name>") {
-
-	Zombie::_id_counter++;
-}
-
-Zombie::Zombie( const Zombie & other ) {
-
-	if (this != &other)
-    {
-		*this = other;
-    }
-}
-
 Zombie::~Zombie( ) {
 
 	std::stringstream	o_stream;
 
 	o_stream << this->_id;
 	std::cout << this->getName() << "_" << o_stream.str() << ": died (again) a horrible death.. \"" << "Gnar'har-chuck'" << "gluck!\"" << std::endl;
-}
-
-Zombie &	Zombie::operator=( Zombie const & rhs ) {
-
-	if (this != &rhs)
-	{
-		this->_string = rhs.getName();
-	}
-	return *this;
 }
 
 ///			Functions/Methods
