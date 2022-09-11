b3e560cc9f41f1444825b9a188336bff2ea71e06
jcorneli <jaybart@protonmail.com>
2022-09-11 10:49:00 +0200

    cleanup

--- module04/ex00/src/Animal.cpp
+++ module04/ex00/src/Animal.cpp
@@ -1,53 +1,62 @@
-#include <iostream>
 #include "Animal.hpp"
 
 ///			Public:
 
 ///			Getters / Setters
 
-std::string		Animal::getType( ) const {
+const std::string &	Animal::getType( ) const {
 
 	return this->type;
 }
 
 ///			Constructor / Destructor
 
-Animal::Animal( std::string type ) : type(type) {
+Animal::Animal( const std::string & type ) : type(type) {
 
 	if (Animal::_loud)
+	{
   		std::cout << "[Animal] Specific constructor called" << std::endl;
+	}
 }
 
 Animal::Animal( ) : type("Shapeless"){
 
 	if (Animal::_loud)
+	{
   		std::cout << "[Animal] Default constructor called" << std::endl;
+	}
 }
 
-Animal::Animal( const Animal& other) {
+Animal::Animal( const Animal & other ) {
 
+	if (Animal::_loud)
+	{
+  		std::cout << "[Animal] Copy constructor called" << std::endl;
+	}
 	if (this != &other)
 	{
 	  *this = other;
 	}
-	if (Animal::_loud)
-  		std::cout << "[Animal] Copy constructor called" << std::endl;
 }
 
 Animal::~Animal( ) {
 
 	if (Animal::_loud)
+	{
   		std::cout << "[Animal] Destructor called" << std::endl;
+	}
 }
 
-Animal &	Animal::operator=( const Animal& rhs ) {
+Animal &	Animal::operator=( const Animal & rhs ) {
 
+	if (Animal::_loud)
+	{
+  		std::cout << "[Animal] Copy assignment operator called" << std::endl;
+	}
 	if (this != &rhs)
 	{
 		this->type = rhs.getType();
 	}
-	if (Animal::_loud)
-  		std::cout << "[Animal] Copy assignment operator called" << std::endl;
 	return *this;
 }
 
@@ -68,7 +77,7 @@ void	Animal::makeSilent( void ) {
 
 bool	Animal::_loud = true;
 
-std::ostream & operator<<( std::ostream & o_stream, const Animal & animal ) {
+std::ostream &	operator<<( std::ostream & o_stream, const Animal & animal ) {
 
 	return o_stream << animal.getType();
 }
